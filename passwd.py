88f67e2d481a5c4b96de2ee084739d1093f8d950
Erwan Keribin <erwan.keribin@gmail.com>
2014-07-16 22:50:26 +0200

    Refactors and comments code as inspired by an example from Guido van Rossum

--- passwd.py
+++ passwd.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python3
+
+"""A strong password generator"""
+
 import argparse
 import hashlib
 import getpass
 
+ARGS = argparse.ArgumentParser(description="Output a strong password")
+ARGS.add_argument("name",type=str)
+ARGS.add_argument("-n",type=int,default=8)
+
 class Chars:
+    """Iterator that yields wanted characters"""
     def __init__(self):
         self.bounds = [(35,37),42,(48,58),(64,72),(74,78),80,(82,90),(97,107),(109,122)]
 
@@ -23,20 +32,27 @@ class Chars:
             return chr(a)
         raise StopIteration
 
-chars = []
-for c in Chars():
-    chars.append(c)
-
-def trad_num(n):
-    (d, m) = divmod(n,len(chars))
-    if d > 0:
-        return trad_num(d) + chars[m]
-    return chars[m]
+    def chars(self):
+        """Returns a list of the characters contained in Chars"""
+        chars = []
+        for c in Chars():
+            chars.append(c)
+        return chars
 
 def trad(s):
+    """Translates a unicode string in a string of Char characters"""
+    chars = Chars().chars()
+
+    def trad_num(n):
+        (d, m) = divmod(n,len(chars))
+        if d > 0:
+            return trad_num(d) + chars[m]
+        return chars[m]
+    
     return trad_num(int("".join([str(c) for c in s])))
 
 def is_secure(s):
+    """Returns if a string is secure i.e. contains all the differents types of Chars"""
     types = [0,0,0,0]
     for c in s:
         if ord(c) >= 48 and ord(c) <= 58:
@@ -52,20 +68,25 @@ def is_secure(s):
         res &= counter > 0
     return res
 
-passwd = hashlib.sha512()
+def main():
+    """Main program.
 
-parser = argparse.ArgumentParser(description="Output a strong password")
-parser.add_argument("name",type=str)
-parser.add_argument("-n",type=int,default=8)
-args = parser.parse_args()
+    Parse arguments, prepare sha512, get master password, return secure password.
+    """
+    args = ARGS.parse_args()
 
-passwd.update(args.name.encode("utf-8"))
-master_passwd = getpass.getpass(prompt="Master password:")
-passwd.update(master_passwd.encode("utf-8"))
+    passwd = hashlib.sha512()
+    passwd.update(args.name.encode('utf-8'))
+    master_passwd = getpass.getpass(prompt="Master password:")
+    passwd.update(master_passwd.encode('utf-8'))
 
-res = trad(passwd.digest())[:args.n]
-while not is_secure(res):
-    passwd.update("*".encode("utf-8"))
     res = trad(passwd.digest())[:args.n]
-print(res)
+    while not is_secure(res):
+        passwd.update('*'.encode('utf-8'))
+        res = trad(passwd.digest())[:args.n]
+
+    return res
+
+if __name__ == '__main__':
+    print(main())
 
